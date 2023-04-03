003db1a0c83f0cba35798d7ff094d39419e56011
Mohammad Faheem <48665968+eddy7896@users.noreply.github.com>
2023-04-03 15:41:31 +0530

    Add files via upload

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2023 Online Library Management System | Designed & Devloped by : Mohammad Faheem & Irfan Sofi  </a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
